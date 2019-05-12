ab0cb33c5c591d59b13702b2f85feec4587038bc
Rostyslav Ostapiuk <rostyslav.ostapiuk@gmail.com>
2019-05-12 19:20:51 +0200

    add firebase config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: 'AIzaSyAN22KrN7lIs2OSPhJ7JaQNVMOaTcH6B6A',
+    authDomain: 'languageapp-8a657.firebaseapp.com',
+    databaseURL: 'https://languageapp-8a657.firebaseio.com',
+    projectId: 'languageapp-8a657',
+    storageBucket: 'languageapp-8a657.appspot.com',
+    messagingSenderId: '669727888631',
+    appId: '1:669727888631:web:9ba07abaf5ca57ee'
+  }
 };
 
 /*
